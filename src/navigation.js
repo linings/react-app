1a69a48f9612e19b5413f75c8550158c76ce4048
linings <petya_pavlova_55@abv.bg>
2021-03-14 22:42:42 +0200

    logout removed

--- src/navigation.js
+++ src/navigation.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
-import Logout from "./components/logout";
 
 const Navigation = () => {
   return (
@@ -13,7 +12,6 @@ const Navigation = () => {
         <Route path="/" exact component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-        <Route path="/logout" component={Logout} />
       </Switch>
     </BrowserRouter>
   );
--- src/utils/authenticate.js
+++ src/utils/authenticate.js
@@ -1,12 +1,29 @@
 const authenticate = async (url, body) => {
   const { username, password, repeatPassword } = body;
 
-  if (repeatPassword) {
-    if (password === repeatPassword) {
+  try {
+    if (repeatPassword) {
+      if (password === repeatPassword) {
+        let promise = await fetch(url, {
+          method: "POST",
+          body: JSON.stringify({
+            email: username,
+            password,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        let response = await promise.json();
+        document.cookie = `x-auth-token=${response.ownerId}`;
+      } else {
+        console.log("Diff passes!");
+      }
+    } else {
       let promise = await fetch(url, {
         method: "POST",
         body: JSON.stringify({
-          email: username,
+          login: username,
           password,
         }),
         headers: {
@@ -14,23 +31,12 @@ const authenticate = async (url, body) => {
         },
       });
       let response = await promise.json();
+      console.log(response);
       document.cookie = `x-auth-token=${response.ownerId}`;
-    } else {
-      console.log("Diff passes!");
     }
-  } else {
-    let promise = await fetch(url, {
-      method: "POST",
-      body: JSON.stringify({
-        login: username,
-        password,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    let response = await promise.json();
-    document.cookie = `x-auth-token=${response.ownerId}`;
+  } catch (error) {
+    console.log(error.message);
+    return;
   }
 };
 
