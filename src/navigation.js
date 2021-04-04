dc4c06d7988c8813716f139ca967542d0a870146
linings <petya_pavlova_55@abv.bg>
2021-04-04 14:46:55 +0300

    Navigation expanded

--- src/navigation.js
+++ src/navigation.js
@@ -1,23 +1,42 @@
 import React from "react";
 
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Home from "./components/homeComponents/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import Donate from "./components/donateComponents/donate";
 import Admin from "./components/admin/adminPage";
 import Messages from "./components/messages";
+import getCookie from "./utils/cookie";
 
 const Navigation = () => {
+  const loggedIn = getCookie('x-auth-token');
+  const isAdmin = localStorage.getItem('isAdmin');
+
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/adopt" component={Donate} />
-        <Route path="/upload" component={Admin} />
-        <Route path="/messages" component={Messages} />
+        <Route path="/login">
+          {loggedIn !== null ? <Redirect to="/" /> : <Login />}
+        </Route>
+        <Route path="/register">
+          {loggedIn !== null ? <Redirect to="/" /> : <Register />}
+        </Route>
+        <Route path="/adopt">
+          {loggedIn === null ? <Redirect to="/login" /> : <Donate />}
+        </Route>
+        <Route path="/messages">
+          {loggedIn === null ? <Redirect to="/login" /> : <Messages />}
+        </Route>
+        <Route path="/upload">
+          {loggedIn === null || isAdmin === 'false' ? <Redirect to="/" /> : <Admin />}
+        </Route>
+        {/* <Route path="/login" component={Login} /> */}
+        {/* <Route path="/register" component={Register} /> */}
+        {/* <Route path="/adopt" component={Donate} /> */}
+        {/* <Route path="/upload" component={Admin} /> */}
+        {/* <Route path="/messages" component={Messages} /> */}
       </Switch>
     </BrowserRouter>
   );
--- src/utils/getUserDataWithRelations.js
+++
@@ -1,8 +0,0 @@
-import RESTAPI from '../REST API'
-
-// https://eu-api.backendless.com/E06230DB-16D9-F7BF-FF7C-08FF9D3F8C00/9CD21057-7F83-4228-B53A-A6F3D2D938F2/data/Users?loadRelations=messages
-const getUserDataWithRelations = async () => {
-    let data = await fetch(RESTAPI.name + `data/Users?loadRelations=messages`);
-    return await data.json();
-}
-export default getUserDataWithRelations;
